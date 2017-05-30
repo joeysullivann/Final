0aa25baf022c1d657555e7211cba901d31b341f6
joeysullivann <joeysull03@gmail.com>
2017-05-30 14:57:11 -0700

    Final Project

--- client/main.js
+++ client/main.js
@@ -3,24 +3,81 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
- Cars = new Mongo.Collection('cars');
-Template.cars.helpers({
-  'cars' : function(){
-    return Cars.find();
-  }
-})
+Session.setDefault('cars', [{
+	make: 'toyota', 
+	model:'corolla', 
+	year:'2015', 
+	color:'white'
+}]);
+
+Template.body.helpers({
+	cars: function (){
+		return Session.get('cars');
+	},
+	test: function (){
+		return Session.get('test');
+	}
+
+});
+
+Template.searchCars.events({
+	'submit #searchCars': function (){
+		event.preventDefault();
+		var make = event.target.makeSearch.value;
+		var model = event.target.modelSearch.value;
+		var year = event.target.yearSearch.value;
+		var color = event.target.colorSearch.value;
+
+		event.target.makeSearch.value = '';
+		event.target.modelSearch.value = '';
+		event.target.yearSearch.value = '';
+		event.target.colorSearch.value = '';
+
+    	Meteor.call('searchForCars', make, model, year, color, function(err,res){ 
+    		if (res.length > 0) {
+    			//set initial result
+    			var makeFound = JSON.stringify(res[0]['make']);
+    			var modelFound = JSON.stringify(res[0]['model']);
+    			var yearFound = JSON.stringify(res[0]['year']);
+    			var colorFound = JSON.stringify(res[0]['color']);
+    			Session.set('cars', [{make:makeFound, model:modelFound, year:yearFound, color:colorFound}]);
+
+    			//set remaining result
+    			var sessionData = Session.get('cars');
+    			for (var i = res.length - 1; i >= 1; i--) {
+    				var makeFound = JSON.stringify(res[i]['make']);
+	    			var modelFound = JSON.stringify(res[i]['model']);
+	    			var yearFound = JSON.stringify(res[i]['year']);
+	    			var colorFound = JSON.stringify(res[i]['color']);
+					sessionData.push({
+						make: makeFound,
+						model: modelFound,
+						year: yearFound,
+						color: colorFound
+					});
+    			}
+    			Session.set('cars', sessionData);
+    		}
+    	});
+		return false;
+	}
+});
+
 Template.addCars.events({
-    'submit form': function(event){
-	    event.preventDefault();
-	    var carMake = $('[name="carMake"]').val();
-	    var carModel = $('[name="carModel"]').val();
-	    var carYear = $('[name="carYear"]').val();
-	    var carColor = $('[name="carColor"]').val();
-	    Cars.insert({
-	        make: carMake,
-	        model: carModel,
-	        year: carYear,
-	        color: carColor
-    });
+	'submit #addCars': function (){
+		event.preventDefault();
+		var sessionData = Session.get("cars");
+		var make = event.target.make.value;
+		var model = event.target.model.value;
+		var year = event.target.year.value;
+		var color = event.target.color.value;
+
+		event.target.make.value = '';
+		event.target.model.value = '';
+		event.target.year.value = '';
+		event.target.color.value = '';
+
+		Meteor.call('addNewCar', make, model, year, color, function(){});
+		return false;
 	}
 });
--- server/main.js
+++ server/main.js
@@ -1,8 +1,27 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+ 	// code to run on server at startup
+	Cars = new Mongo.Collection('cars');
 });
-Cars = new Mongo.Collection('cars');
-Cars.insert({make:"toyota", model:"corolla", year:"2015", color:"white"});
 
+Meteor.methods({
+  'searchForCars': function (makeSearch, modelSearch, yearSearch, colorSearch) {
+	var searchResult = Cars.find({$and: [{make: makeSearch}, {model:modelSearch}, {year:yearSearch}, {color:colorSearch}]}).fetch();
+	// console.log("found: ", searchResult);
+	if (searchResult.length > 0) {
+		return searchResult;
+	}
+	searchResult = Cars.find({$or: [{make: makeSearch}, {model:modelSearch}, {year:yearSearch}, {color:colorSearch}]}).fetch();
+	return searchResult;
+  }
+});
+
+Meteor.methods({
+  'addNewCar': function (makeAdd, modelAdd, yearAdd, colorAdd) {
+	Cars.insert({make:makeAdd, model:modelAdd, year:yearAdd, color:colorAdd});
+	// console.log("added: ", makeAdd, modelAdd, yearAdd, colorAdd);
+    return 0;
+  }
+});
